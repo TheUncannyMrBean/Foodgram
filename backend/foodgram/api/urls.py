40d82e84e8b584bf468dabbb2c62cf88f1040525
TheUncannyMrBean <theuncannymrbean@pop-os.localdomain>
2023-05-05 03:51:14 +0700

    Второй этап

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -3,15 +3,16 @@ from rest_framework.routers import DefaultRouter
 
 from . import views
 
+app_name = 'api'
+
 router = DefaultRouter()
-router.register(r'users', views.CustomUserViewSet, basename='users')
-router.register(r'recipes', views.RecipeViewSet, basename='recipes')
-router.register(r'tags', views.TagViewSet, basename='tags')
+router.register('users', views.CustomUserViewSet, basename='users')
+router.register('recipes', views.RecipeViewSet, basename='recipes')
+router.register('tags', views.TagViewSet, basename='tags')
 router.register(
-    r'ingredients', views.IngredientViewSet, basename='ingredients')
+    'ingredients', views.IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include('djoser.urls')),
-    path(r'auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
